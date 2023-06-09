9811ba1415c8adeae439243ca70e3e61c3173e37
puerchik <rumyantsevnn@yandex.ru>
2023-06-09 16:57:01 +0300

    added Igor's version

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -12,6 +12,13 @@ type AffairPropsType = {
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
         // need to fix
+        // пропс.функция(мне нужен _id)
+        // давайте проследим боевой путь это функции, или как она будет всплывать:
+        // открывай в нескольких окнах и следи:
+        // отсюда она всплывет в компоненту Affairs вместе с _id ->
+        // далее из Affairs всплывет в HW2->
+        // в HW2 находим deleteAffairCallback- это и есть наш клиент ->
+        // deleteAffairCallback вызовет setAffairs(...) и   deleteAffair(...)
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -25,18 +32,20 @@ function Affair(props: AffairPropsType) {
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
-
+                {/* ПРОПС.ВЫВОДИМ ИМЯ*/}
                 {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
-                {props.affair.priority}
+                {/*создаёт студент*/}
+
+                {/**/}
             </div>
 
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
                 // need to fix
-
+                //ОНКЛИК={ФУНКЦИЯ}
             >
                 {/*текст кнопки могут изменить студенты*/}
                 X
--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -24,7 +24,7 @@ const HW13 = () => {
         const url =
             x === null
                 ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
-                : 'https://samurai.it-incubator.io/api/3.0/homework/test'
+                : 'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test'
 
         setCode('')
         setImage('')
