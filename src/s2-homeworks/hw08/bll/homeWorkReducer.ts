ca9bbe011d745ee7504e2a1b6ebc457aa09a060b
puerchik <rumyantsevnn@yandex.ru>
2023-09-22 14:26:14 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,24 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const sortArray = [...state.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase()
+                ? action.payload === 'up' ? -1 : 1
+                : a.name.toLowerCase() === b.name.toLowerCase()
+                    ? 0
+                    : action.payload === 'up' ? 1 : -1
+            )]
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return sortArray // need to fix
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter(f => f.age >= 18)
         }
         default:
             return state
