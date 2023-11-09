867965e11c98b0735aed92e7abe96f5d1f0512cb
puerchik <rumyantsevnn@yandex.ru>
2023-11-09 11:14:37 +0300

    HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppStoreType } from './bll/store'
+import { loadingAC } from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
-import {Loader} from './Loader'
+import s from './Loader.module.css'
+import { Loader } from './Loader'
 
 /*
 * 1 - в файле loadingReducer.ts дописать типы и логику
@@ -14,32 +15,35 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading);
+    const dispatch = useDispatch();
 
-        // setTimeout
+    const setLoading = () => {
+        dispatch(loadingAC(!isLoading))
+
+        setTimeout(() => dispatch(loadingAC(isLoading)), 1500)
     }
 
     return (
         <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
-
-            <div className={s2.hw}>
-                {isLoading ? (
-                    <div id={'hw10-loading'}>
-                        <Loader/>
-                    </div>
-                ) : (
-                    <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
-                    >
-                        Set loading...
-                    </SuperButton>
-                )}
+            <div className={`${s2.hwTitle} ${s.title}`}>Homework #10</div>
+            <div className={`${s2.hw} ${s.buttonWrapper}`}>
+                <div className={s2.container}>
+                    {isLoading ? (
+                        <div id={'hw10-loading'}>
+                            <Loader />
+                        </div>
+                    ) : (
+                        <SuperButton
+                            id={'hw10-button-start-loading'}
+                            xType=''
+                            onClick={setLoading}
+                        >
+                            Set loading...
+                        </SuperButton>
+                    )}
+                </div>
             </div>
         </div>
     )
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,21 +2,19 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof initState => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
 
         default:
             return state
     }
 }
 
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
-
-export const loadingAC = (isLoading: boolean): LoadingActionType => ({
+export const loadingAC = (isLoading: boolean) => ({
     type: 'CHANGE_LOADING',
     isLoading,
-})
+} as const)
+
+type LoadingActionType = ReturnType<typeof loadingAC>
